dc714dbf757bd5b5f11f22115b6a2146a04f714d
Najja <nkossally@gmail.com>
2019-03-27 17:38:33 -0400

    Pass 20 of 61 tests for minimum path sum.

--- minimum_path_sum.js
+++ minimum_path_sum.js
@@ -1,25 +1,21 @@
 var minPathSum = function(grid) {
-  return minimumPathSumRecursion(grid, 0, 0, 0, null);
+  return minPathSumRecursion(grid, 0, 0, 0, []);
 };
 
-var minPathSumRecursion = function(grid, row, col, sum, min) {
+var minPathSumRecursion = function(grid, row, col, sum, sums) {
   sum += grid[row][col];
-  if (min && min < sum) {
-    return null;
-  }
-  if (
-    row === grid.length - 1 &&
-    col === grid[0].length - 1 &&
-    (min === null || sum < min)
-  ) {
-    return sum;
+
+  if (row === grid.length - 1 && col === grid[0].length - 1) {
+    sums.push(sum);
   }
   if (row < grid.length - 1) {
-    minPathSumRecursion(grid, row + 1, col, sum, min);
+    minPathSumRecursion(grid, row + 1, col, sum, sums);
   }
-  if (row < grid[0].length - 1) {
-    minPathSumRecursion(grid, row, col + 1, sum, min);
+  if (col < grid[0].length - 1) {
+    minPathSumRecursion(grid, row, col + 1, sum, sums);
   }
+
+  return Math.min(...sums);
 };
 
 console.log(minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]));
