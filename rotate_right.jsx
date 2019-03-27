8f34238ddce87174f00b550c400a73016ec1b81d
Najja <nkossally@gmail.com>
2019-03-27 12:02:31 -0400

    Solve Rotate Right.

--- rotate_right.jsx
+++ rotate_right.jsx
@@ -1,4 +1,16 @@
 var rotateRight = function(head, k) {
+  if (!head) {
+    return null;
+  }
+
+  length = 0;
+  pointer = head;
+  while (pointer) {
+    length += 1;
+    pointer = pointer.next;
+  }
+  k = k % length;
+
   pointer1 = head;
   pointer2 = head;
   i = 0;
@@ -13,7 +25,22 @@ var rotateRight = function(head, k) {
     pointer1 = pointer1.next;
     pointer2 = pointer2.next;
   }
+
   pointer1.next = head;
   head = pointer2.next;
-  pointer2.next = NULL;
+  pointer2.next = null;
+  return head;
 };
+
+class Node {
+  constructor(val, next = null) {
+    (this.val = val), (this.next = next);
+  }
+}
+node5 = new Node(5);
+node4 = new Node(4, node5);
+node3 = new Node(3, node4);
+node2 = new Node(2, node3);
+node1 = new Node(1, node2);
+
+console.log(rotateRight(node1, 2));
