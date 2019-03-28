162adb551fbadc8fff033e571d79ccace3bc2a2f
Najja <nkossally@gmail.com>
2019-03-28 11:44:23 -0400

    Pass 60 of 61 tests by keeping track of cells already seen.

--- minimum_path_sum.js
+++ minimum_path_sum.js
@@ -1,18 +1,28 @@
 var minPathSum = function(grid) {
-  return minPathSumRecursion(grid, 0, 0, 0, []);
+  return minPathSumRecursion(grid, 0, 0, 0, [], {});
 };
 
-var minPathSumRecursion = function(grid, row, col, sum, sums) {
+var minPathSumRecursion = function(grid, row, col, sum, sums, seenCells) {
   sum += grid[row][col];
+  let key = [row, col];
+  if (seenCells[key]) {
+    if (sum < seenCells[key]) {
+      seenCells[key] = sum;
+    } else {
+      return;
+    }
+  } else {
+    seenCells[key] = sum;
+  }
 
   if (row === grid.length - 1 && col === grid[0].length - 1) {
     sums.push(sum);
   }
   if (row < grid.length - 1) {
-    minPathSumRecursion(grid, row + 1, col, sum, sums);
+    minPathSumRecursion(grid, row + 1, col, sum, sums, seenCells);
   }
   if (col < grid[0].length - 1) {
-    minPathSumRecursion(grid, row, col + 1, sum, sums);
+    minPathSumRecursion(grid, row, col + 1, sum, sums, seenCells);
   }
 
   return Math.min(...sums);
