1e1a6fa531bd3a93ee358c6cb6bdec1d71c7b910
Najja <nkossally@gmail.com>
2019-04-06 20:22:36 -0400

    Start scramble string.

--- maximal_rectangle.rb
+++ maximal_rectangle.rb
@@ -1,43 +1,38 @@
 require "byebug"
+require "set.rb"
 def maximal_rectangle(matrix)
+  zeros = Set.new
+  for row in (0...matrix.length)
+    for col in (0...matrix[0].length)
+      if matrix[row][col] == "0"
+        zeros.add([row, col])
+      end
+    end
+  end
   max = nil
   arr = []
   for row in (0...matrix.length)
     for col in (0...matrix[0].length)
-
-      if(matrix[row][col] == "1")
-        max_row = row
-        max_col = col 
-        while(max_row<matrix.length-1 && matrix[max_row+1][col] == "1")
-          max_row += 1
-        end
-        while( max_col<matrix[0].length-1 && matrix[row][max_col+1] == "1")
-          max_col += 1
-        end
-
-        for test_row in (row..max_row)
-          for test_col in (col..max_col)
-
-            area = (test_row-row+1)*(test_col-col+1)
-            if !max || area>max
-              if all_ones(matrix, row, col, test_row, test_col)
-                max = area
-              end
+      for max_row in (row...matrix.length)
+        for max_col in (col...matrix[0].length)
+          area = (max_row-row+1)*(max_col-col+1)
+          if !max || area>max
+            if all_ones(zeros, row, col, max_row, max_col)
+              max = area
             end
           end
         end
       end
     end
   end
-
 max || 0
-    
+
 end
 
-def all_ones(matrix, start_row, start_col, end_row, end_col)
+def all_ones(zeros, start_row, start_col, end_row, end_col)
   for row in (start_row..end_row)
     for col in (start_col..end_col)
-      if matrix[row][col] == "0"
+      if zeros.include?([row, col])
         return false
       end 
     end
@@ -56,3 +51,61 @@ test_arr = [["1","1","1","1","1","1","1","1"],["1","1","1","1","1","1","1","0"],
 
 p maximal_rectangle(test_arr)
 p all_ones(test_arr, 0,0, 2, 6 )
+
+# def maximal_rectangle(matrix)
+#   max = nil
+#   arr = []
+#   for row in (0...matrix.length)
+#     for col in (0...matrix[0].length)
+
+#       if(matrix[row][col] == "1")
+#         max_row = row
+#         max_col = col 
+#         while(max_row<matrix.length-1 && matrix[max_row+1][col] == "1")
+#           max_row += 1
+#         end
+#         while( max_col<matrix[0].length-1 && matrix[row][max_col+1] == "1")
+#           max_col += 1
+#         end
+
+#         for test_row in (row..max_row)
+#           for test_col in (col..max_col)
+
+#             area = (test_row-row+1)*(test_col-col+1)
+#             if !max || area>max
+#               if all_ones(matrix, row, col, test_row, test_col)
+#                 max = area
+#               end
+#             end
+#           end
+#         end
+#       end
+#     end
+#   end
+
+# max || 0
+    
+# end
+
+# def all_ones(matrix, start_row, start_col, end_row, end_col)
+#   for row in (start_row..end_row)
+#     for col in (start_col..end_col)
+#       if matrix[row][col] == "0"
+#         return false
+#       end 
+#     end
+#   end
+#   return true
+
+# end
+# p maximal_rectangle([
+#   ["1","0","1","0","0"],
+#   ["1","0","1","1","1"],
+#   ["1","1","1","1","1"],
+#   ["1","0","0","1","0"]
+# ])
+
+# test_arr = [["1","1","1","1","1","1","1","1"],["1","1","1","1","1","1","1","0"],["1","1","1","1","1","1","1","0"],["1","1","1","1","1","0","0","0"],["0","1","1","1","1","0","0","0"]]
+
+# p maximal_rectangle(test_arr)
+# p all_ones(test_arr, 0,0, 2, 6 )
---
+++ scramble_string.rb
@@ -0,0 +1,63 @@
+require "byebug"
+def is_scramble(s1, s2)
+  if s1.length <= 1 && s1 == s2
+    return true
+  end
+
+  if same_letters(s1, s2)
+    s1_left = s1[0...s1.length/2]
+    s1_right = s1[s1.length/2..-1]
+    s2_left = s2[0...s2.length/2]
+    s2_right = s2[s2.length/2..-1]
+    if is_scramble(s1_left, s2_left) && is_scramble(s1_right, s2_right)
+      return true
+    end
+    if s1.length % 2 == 0
+      if is_scramble(s1_left, s2_right) && is_scramble(s1_right, s2_left)
+        return true
+      end
+    end
+    if s1.length % 2 == 1
+      s1_left = s1[0..s1.length/2]
+      s1_right = s1[s1.length/2+1..-1]
+      if is_scramble(s1_left, s2_right) && is_scramble(s1_right, s2_left)
+        return true
+      end
+      s2_left = s2[0..s2.length/2]
+      s2_right = s2[s2.length/2+1..-1]
+      if is_scramble(s1_left, s2_left) && is_scramble(s1_right, s2_right)
+        return true
+      end
+      s1_left = s1[0...s1.length/2]
+      s1_right = s1[s1.length/2..-1]
+      if is_scramble(s1_left, s2_right) && is_scramble(s1_right, s2_left)
+        return true
+      end
+    end
+  end
+  return false
+end
+
+def same_letters(s1, s2)
+  if s1.length != s2.length
+    return false
+  end
+
+  idx = 0
+  while idx < s1.length
+    char = s1[idx]
+    idx2 = s2.index(char)
+    if idx2
+      s2 = s2[0...idx2] + s2[idx2+1..-1]
+    else
+      return false
+    end
+    idx += 1
+  end
+
+  return s2 == ""
+end
+
+p is_scramble("great", "rgeat")
+p is_scramble("abcde", "caebd")
+
