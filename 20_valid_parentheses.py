e3c0eab86e9ecdb3b162409b0771d56f72432193
Najja <nkossally@gmail.com>
2019-07-04 14:34:09 -0400

    Finish valid parentheses

--- 20_valid_parentheses.py
+++ 20_valid_parentheses.py
@@ -1,25 +1,24 @@
 class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        matches = {
-          ')' :'(',
-          ']' : '[',
-          '}' : '{',
-        }
-        stack = []
-        for char in s:
-          if char in matches.values():
-            stack.append(char)
-          else:
-            if matches[char] is stack[-1]:
-              stack.pop()
-            else:
-              return False
-        return len(stack) == 0
+  def isValid(self, s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    matches = {
+      ')' :'(',
+      ']' : '[',
+      '}' : '{',
+    }
+    stack = []
+    for char in s:
+      if char in matches.values():
+        stack.append(char)
+      else:
+        if len(stack) > 0 and matches[char] == stack[len(stack)-1]:
+          stack.pop()
+        else:
+          return False
+    return len(stack) == 0
 
 test = Solution()
 print(test.isValid("()"))
-
