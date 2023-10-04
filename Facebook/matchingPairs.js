28cd7fb2230690009e1b43de851e3c22093efb77
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2023-10-04 12:21:55 -0400

    algorithm

---
+++ Facebook/matchingPairs.js
@@ -0,0 +1,101 @@
+
+// Add any helper functions you may need here
+
+
+function matchingPairs(s, t) {
+    const matchingIndices = new Set();
+    for(let i = 0; i < s.length; i++){
+      if(s[i] === t[i]) matchingIndices.add(i)
+     }
+    
+    let canIncreaseByOne = false;
+    let canIncreaseByTwo = false;
+    let twoDiffIdxMatch = false
+    
+    const getCanIncrease = () =>{
+      let result = false;
+      for(let i = 0; i < s.length; i++){
+        for(let j = 0; j < t.length; j++){
+          if(i !== j && s[i] === t[j]) twoDiffIdxMatch = true;
+  
+          if(!matchingIndices.has(i) && !matchingIndices.has(j)){
+            if(s[i] === t[j]) canIncreaseByOne = true;
+            if(s[i] === t[j] && s[j] === t[i]){
+              canIncreaseByTwo = true;
+            }
+          }
+        }
+        
+      }
+    }
+    getCanIncrease();
+    if(matchingIndices.size === s.length){
+      if(twoDiffIdxMatch){
+        return matchingIndices.size
+      } else {
+        return matchingIndices.size - 2;
+      }
+    }
+    if(canIncreaseByTwo) return matchingIndices.size + 2;
+    if(canIncreaseByOne)return matchingIndices.size + 1;
+  
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // These are the tests we use to determine if the solution is correct.
+  // You can add your own at the bottom.
+  function printInteger(n) {
+    var out = '[' + n + ']';
+    return out;
+  }
+  
+  var test_case_number = 1;
+  
+  function check(expected, output) {
+    var result = (expected == output);
+    var rightTick = "\u2713";
+    var wrongTick = "\u2717";
+    if (result) {
+      var out = rightTick + ' Test #' + test_case_number;
+      console.log(out);
+    }
+    else {
+      var out = '';
+      out += wrongTick + ' Test #' + test_case_number + ': Expected ';
+      out += printInteger(expected);
+      out += ' Your output: ';
+      out += printInteger(output);
+      console.log(out);
+    }
+    test_case_number++;
+  }
+  
+  var s_1 = "abcde";
+  var t_1 = "adcbe";
+  var expected_1 = 5;
+  var output_1 = matchingPairs(s_1, t_1);
+  check(expected_1, output_1);
+  
+  var s_2 = "abcd";
+  var t_2 = "abcd";
+  var expected_2 = 2;
+  var output_2 = matchingPairs(s_2, t_2);
+  check(expected_2, output_2); 
+  
+  // Add your own test cases here
+  
+  var s_3 = "mno";
+  var t_3 = "mno";
+  var expected_3 = 1;
+  var output_3 = matchingPairs(s_3, t_3);
+  check(expected_3, output_3); 
+  
