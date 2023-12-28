558938a3517c431a5aa916e0d7630ae3a76aa631
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2023-12-28 17:04:46 -0500

    complete data annotation assessment

--- Misc/dataAnnotationChallenge.js
+++ Misc/dataAnnotationChallenge.js
@@ -1,85 +1,53 @@
 const input = "./input.txt";
+const input2 = "./Input2.txt";
 const fs = require("fs");
 
 const decode_message = async (inputFile) => {
-  let arr;
-  fs.readFile(inputFile, (err, data) => {
-    if (err) throw err;
-
-    arr = data.toString().split("\n");
-
-    const numToWord = {};
-    arr.forEach((str) => {
-      if (str.length > 0) {
-        const numAndWordArr = str.split(" ");
-        numToWord[numAndWordArr[0]] = numAndWordArr[1];
-      }
-    });
-
-    const sortedNums = Object.keys(numToWord)
-      .map((numStr) => parseInt(numStr))
-      .sort((a, b) => a - b);
-      console.log(numToWord)
-
-    let pyramidRowLength = 1;
-    let idx = -1;
-
-    let message = "";
-
-    while (idx < sortedNums.length) {
-      idx += pyramidRowLength;
-      if (idx < sortedNums.length) {
-        message += numToWord[sortedNums[idx]] + " ";
-      }
-      pyramidRowLength++;
+  async function readFile() {
+    let content;
+    try {
+      content = await fs.promises.readFile(inputFile, "utf8");
+    } catch (e) {
+      console.error(e);
     }
-    console.log(message)
-    return message;
+    return content;
+  }
+
+  const data = await readFile();
+  const arr = data.toString().split("\n");
+
+  const numToWord = {};
+  arr.forEach((str) => {
+    if (str.length > 0) {
+      const numAndWordArr = str.split(" ");
+      numToWord[numAndWordArr[0]] = numAndWordArr[1];
+    }
   });
+
+  const sortedNums = Object.keys(numToWord)
+    .map((numStr) => parseInt(numStr))
+    .sort((a, b) => a - b);
+
+  let pyramidRowLength = 1;
+  let idx = -1;
+  let message = "";
+
+  while (idx < sortedNums.length) {
+    idx += pyramidRowLength;
+    if (idx < sortedNums.length) {
+      message += numToWord[sortedNums[idx]] + " ";
+    }
+    pyramidRowLength++;
+  }
+  console.log(message);
+  return message;
 };
 
-decode_message(input);
 
-
-
-
-
-// const decode_message = async (inputFile) => {
-//     let arr;
-//     fs.readFile(inputFile, (err, data) => {
-//       if (err) throw err;
-  
-//       arr = data.toString().split("\n");
-  
-//       const numToWord = {};
-//       arr.forEach((str) => {
-//         if (str.length > 0) {
-//           const numAndWordArr = str.split(" ");
-//           numToWord[numAndWordArr[0]] = numAndWordArr[1];
-//         }
-//       });
-  
-//       const sortedNums = Object.keys(numToWord)
-//         .map((numStr) => parseInt(numStr))
-//         .sort((a, b) => a - b);
-//         console.log(numToWord)
-  
-//       let pyramidRowLength = 1;
-//       let idx = -1;
-  
-//       let message = "";
-  
-//       while (idx < sortedNums.length) {
-//         idx += pyramidRowLength;
-//         if (idx < sortedNums.length) {
-//           message += numToWord[sortedNums[idx]] + " ";
-//         }
-//         pyramidRowLength++;
-//       }
-//       console.log(message)
-//       return message;
-//     });
-//   };
-  
-//   decode_message(input);
-  
+/* This function decodes a message by putting each line of the input file into an array,
+ * and then constructiong an object for which the keys are the number on each line,
+ * and the values are the words associated with each key. Finally, the function
+ * constructs the decoded message by starting with the word associated with 1,
+ * adding the word associated with 1 + 2 = 3, then adding the word associated
+ * with 3 + 3 = 6, and so on.
+ */
