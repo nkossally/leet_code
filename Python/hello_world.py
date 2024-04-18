d95907cfcf3c4e3f4260dd530bd4953e8054c7d9
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2024-04-18 16:57:08 -0400

    python

--- Python/hello_world.py
+++ Python/hello_world.py
@@ -65,3 +65,63 @@ print(astring.endswith("asdfasdfasdf"))
 astring = "Hello world!"
 afewwords = astring.split(" ")
 print(afewwords)
+
+
+# Prints out 0,1,2,3,4 and then it prints "count value reached 5"
+
+count=0
+while(count<5):
+    print(count)
+    count +=1
+else:
+    print("count value reached %d" %(count))
+
+# Prints out 1,2,3,4
+for i in range(1, 10):
+    if(i%5==0):
+        break
+    print(i)
+else:
+    print("this is not printed because for loop is terminated because of break but not due to fail in condition")
+
+
+
+
+
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+# your code goes here
+car1 = Vehicle()
+car1.name = "car1"
+car1.kind = "convertible"
+car2 = Vehicle()
+
+# test code
+print(car1.description())
+print(car2.description())
+
+
+# define the Vehicle class
+class AnotherVehicle:
+    def __init__(self, name, kind, color, value):
+        self.name = name
+        self.kind = kind
+        self.color = color
+        self.value = value
+    def __str__():
+        return "hello whatever no"
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+# your code goes here
+car3 = AnotherVehicle("car3", "van", "purple", 5)
+
+print(car3.description())
+# print(car3)
