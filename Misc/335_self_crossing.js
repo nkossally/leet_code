ad4a0b3cd41add35452353e6110cd4300c76c203
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2024-06-23 20:27:05 -0400

    algo

--- Misc/335_self_crossing.js
+++ Misc/335_self_crossing.js
@@ -50,3 +50,29 @@ var isSelfCrossing = function (distance) {
 
     return hasOverlap
 };
+
+
+/**
+ * @param {number[]} distance
+ * @return {boolean}
+ */
+var isSelfCrossingFaster = function (distance) {
+    let result = false
+    for( let i = 3; i < distance.length; i++){
+        const dist = distance[i]
+        if(distance[i] >= distance[i- 2] && distance[i - 1] <= distance[i - 3]){
+            result = true
+            break
+        }
+        if(i >= 4 && distance[i - 1] === distance[i- 3] && distance[i - 2] <= distance[i] + distance[i - 4]){
+            result = true
+            break
+        }
+
+        if(i >= 5 && distance[i - 4] <= distance[i -2] && distance[i] + distance[i - 4] >= distance[i -2] && distance[i - 3] >= distance[i- 1] && distance[i - 5] >= distance[i - 3] - distance[i - 1]){
+            result = true
+            break
+        }
+    }
+    return result
+};
