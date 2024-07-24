9cee788d23752bc487987caedbd58220c9700e9d
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2024-07-24 10:30:41 -0400

    add more promises

--- Javascript/promise.js
+++ Javascript/promise.js
@@ -13,27 +13,27 @@
 //     console.log("This is in the catch " + message)
 // })
 
-const userLeft = true;
+const userLeft = false;
 const userWatchingCatMeme = false;
 
-function watchTutorialCallback(callback, errorCallback) {
-    if(userLeft){
-        errorCallback(
-            {
-                name: "User Watching Cat Meme",
-                message: "like cats"
-            }
-        )
-    } else {
-        callback("Hey it worked")
-    }
-}
+// function watchTutorialCallback(callback, errorCallback) {
+//     if(userLeft){
+//         errorCallback(
+//             {
+//                 name: "User Watching Cat Meme",
+//                 message: "like cats"
+//             }
+//         )
+//     } else {
+//         callback("Hey it worked")
+//     }
+// }
 
 // watchTutorialCallback((message) => {
 //     console.log("Success " + message)
 
 // }, error =>{
-//     console.log(error.name + " "  + error.message)
+//     console.log("error is" +  " " + error.name + " "  + error.message)
 // })
 
 function watchTutorialPromise() {
@@ -44,52 +44,162 @@ function watchTutorialPromise() {
         message: "oh no",
       });
     } else if (userWatchingCatMeme) {
-        reject({
-            name: "User watching cat meme",
-            message: "like cats",
-          });
+      reject({
+        name: "User watching cat meme",
+        message: "like cats",
+      });
     } else {
-      resolve("Hey it worked");
+      return resolve({ name: "Hey it worked", message: "ello" });
     }
   });
 }
 
-watchTutorialPromise((message) => {
-    console.log("Success " + message)
+// watchTutorialPromise()
+//   .then((message) => console.log("meow", message))
+//   .catch((error) => {
+//     console.log(error.name + " " + error.message);
+//   });
 
-}).catch((error) =>{
-    console.log(error.name + " " + error.message)
+const recordVideo1 = new Promise((resolve, reject) => {
+  resolve("Video 1 recorded");
+});
+
+const recordVideo2 = new Promise((resolve, reject) => {
+  resolve("Video 2 recorded");
+});
+
+const recordVideo3 = new Promise((resolve, reject) => {
+  resolve("Video 3 recorded");
+});
+
+// Promise.all([
+//     recordVideo1,
+//     recordVideo2,
+//     recordVideo3
+// ]).then(messages =>{
+//     console.log(messages)
+// })
+// Promise.race([
+//     recordVideo1,
+//     recordVideo2,
+//     recordVideo3
+// ]).then(message =>{
+//     console.log(message)
+// })
+
+// const arrgg = [1, 2, 3, 4, "hey"];
+// console.log(arrgg.find(elem => elem === 4))
+// console.log(arrgg.find(elem => elem[0] === "h"))
+// console.log(arrgg.find(elem => elem === 5))
+
+// new Promise((resolveOuter) => {
+//     resolveOuter(
+//       new Promise((resolveInner) => {
+//         setTimeout(resolveInner, 1000);
+//       }),
+//     );
+//   });
+
+// new Promise((resolveOuter) => {
+//     resolveOuter(
+//       new Promise((resolveInner) => {
+//         setTimeout(() => resolveInner(console.log("pizza")), 2000);
+//       }),
+//     );
+//   });
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("foo");
+  }, 300);
+});
+
+const aThenable = {
+  then(onFulfilled, onRejected) {
+    onFulfilled({
+      // The thenable is fulfilled with another thenable
+      then(onFulfilled, onRejected) {
+        onFulfilled(console.log(42));
+      },
+    });
+  },
+};
+
+// Promise.resolve(aThenable); // A promise fulfilled with 42
+
+const promiseWrapper = () => {
+  const rand = Math.floor(100 * Math.random());
+  console.log(rand);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (rand % 2 === 0) {
+        resolve("It's even!");
+      } else {
+        reject("It's odd");
+      }
+    }, 1000);
+  });
+};
+
+// promiseWrapper()
+//   .then((message) => {
+//     console.log("success" + " " + message);
+//   })
+//   .catch((err) => {
+//     console.log("oh no!" + " " + err);
+//   });
+
+const wrapAroundTheWrap = async () => {
+  try {
+    const result = await promiseWrapper();
+    console.log("the success message is " + result);
+  } catch (err) {
+    console.log("the error is " + err);
+  }
+};
+
+// wrapAroundTheWrap();
+
+
+const resolvedPromise = new Promise((resolve, reject) =>{
+  resolve("it worked!")
 })
 
-
-const recordVideo1 = new Promise((resolve, reject) =>{
-    resolve("Video 1 recorded")
+const rejecteddPromise = new Promise((resolve, reject) =>{
+  reject("it did not work!")
 })
 
-const recordVideo2 = new Promise((resolve, reject) =>{
-    resolve("Video 2 recorded")
+const rejecteddPromise2 = new Promise((resolve, reject) =>{
+  reject("the second did not work!")
 })
 
-const recordVideo3 = new Promise((resolve, reject) =>{
-    resolve("Video 3 recorded")
+Promise.race([
+  rejecteddPromise2,
+  rejecteddPromise,
+  resolvedPromise,
+]).then(message =>{
+  console.log(message)
+}).catch(err =>{
+  console.log(err)
 })
 
 Promise.all([
-    recordVideo1,
-    recordVideo2,
-    recordVideo3
-]).then(messages =>{
-    console.log(messages)
-})
-Promise.race([
-    recordVideo1,
-    recordVideo2,
-    recordVideo3
+  rejecteddPromise2,
+  resolvedPromise,
+  rejecteddPromise
 ]).then(message =>{
-    console.log(message)
+  console.log(message)
+}).catch(err =>{
+  console.log(err)
 })
 
-const arrgg = [1, 2, 3, 4, "hey"];
-console.log(arrgg.find(elem => elem === 4))
-console.log(arrgg.find(elem => elem[0] === "h"))
-console.log(arrgg.find(elem => elem === 5))
+
+Promise.any([
+  rejecteddPromise2,
+  resolvedPromise,
+  rejecteddPromise
+]).then(message =>{
+  console.log(message)
+}).catch(err =>{
+  console.log(err)
+})
