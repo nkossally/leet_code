f026ad0ef08591dc6479d2687e8cacfdd4534fde
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-06-30 18:25:09 -0400

    algo

---
+++ Python/ Code   Testcase Testcase Test Result 647. Palindromic Substrings.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        def get_count(string, left, right):
+            count = 0
+
+            while left >= 0 and right < len(string) and string[right] == string[left]:
+                count += 1
+                left -= 1
+                right += 1
+            return count
+
+        total = 0
+        for i in range(len(s)):
+            total += get_count(s, i, i)
+            total += get_count(s, i, i + 1)
+        return total
