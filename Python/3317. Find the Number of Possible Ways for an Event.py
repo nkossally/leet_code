d7d0d53e1dc740d7619a65f363d23489579086fb
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-07-03 07:02:27 -0400

    algo

--- Python/3317. Find the Number of Possible Ways for an Event.py
+++ Python/3317. Find the Number of Possible Ways for an Event.py
@@ -25,3 +25,34 @@ class Solution(object):
 
         is_occupied_arr = [False for _ in range(x)]
         return helper(0, is_occupied_arr, 0)
+    
+    class Solution:
+    def numberOfWays_2(self, n: int, x: int, y: int) -> int:
+        
+        mod = 10 ** 9 + 7
+
+        def perm(num, lower):
+            bound = num - lower
+            curr = num
+            result = 1
+            while curr > bound:
+                result *= curr
+                curr -= 1
+            return result % mod
+
+        def get_count(num, groups):
+            if groups > num:
+                return 0
+            if groups == 1:
+                return 1
+            return (get_count(num -1, groups - 1) + groups * get_count(num - 1, groups)) % mod
+
+        min_groups = min(x, n)
+
+
+        total = 0
+        for i in range(1, min_groups + 1):
+            total += math.perm(x, i) * (y ** i) * get_count(n, i)
+            total = total % mod
+        return total
+                
