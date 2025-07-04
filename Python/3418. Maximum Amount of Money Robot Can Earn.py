d12cd49b4ed5e4da8d1b59c5c55045bc8103fef9
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-07-04 11:04:01 -0400

    algo

--- Python/3418. Maximum Amount of Money Robot Can Earn.py
+++ Python/3418. Maximum Amount of Money Robot Can Earn.py
@@ -3,28 +3,9 @@ class Solution:
         def is_on_board(x, y):
             return x >= 0 and x < len(coins) and y >= 0 and y < len(coins[0])
         
-        def get_one_away(x, y):
-            way_1 = dp[x - 1][y][0] if is_on_board(x - 1, y) else float("-inf")
-            way_2 = dp[x ][y - 1][0] if is_on_board(x, y - 1) else float("-inf")
-            max_num = max(way_1, way_2)
-            if max_num == float("-inf"):
-                return 0
-            return max_num
-
-        def get_one_skip(x, y):
-            way_1 = dp[x - 1][y][1] if is_on_board(x - 1, y) else float("-inf")
-            way_2 = dp[x ][y - 1][1] if is_on_board(x, y - 1) else float("-inf")
-            max_num = max(way_1, way_2)
-            if max_num == float("-inf"):
-                return 0
-            return max_num
-
-
-        def get_two_skips(x, y):
-            way_1 = dp[x - 1][y][2] if is_on_board(x - 1, y) else float("-inf")
-            way_2 = dp[x ][y - 1][2] if is_on_board(x, y - 1) else float("-inf")
-            way_3 = dp[x - 1][y - 1][2] if is_on_board(x - 1, y - 1) else float("-inf")
-
+        def get_dp(x, y, index):
+            way_1 = dp[x - 1][y][index] if is_on_board(x - 1, y) else float("-inf")
+            way_2 = dp[x ][y - 1][index] if is_on_board(x, y - 1) else float("-inf")
             max_num = max(way_1, way_2)
             if max_num == float("-inf"):
                 return 0
@@ -32,16 +13,14 @@ class Solution:
 
         dp = [[None for _ in range(len(coins[0])) ] for _ in range(len(coins))]
 
-    
         for i in range(len(coins)):
             for j in range(len(coins[0])):
-                one_away = get_one_away(i, j)
-                one_skip = get_one_skip(i, j)
-                two_skip = get_two_skips(i, j)
-                elem_1 = coins[i][j] + one_away
-                elem_2 = max(one_away, one_skip + coins[i][j])
+                no_skip = get_dp(i, j, 0)
+                one_skip = get_dp(i, j, 1)
+                two_skip = get_dp(i, j, 2)
+                elem_1 = coins[i][j] + no_skip
+                elem_2 = max(no_skip, one_skip + coins[i][j])
                 elem_3 = max( one_skip, two_skip + coins[i][j])
-
 
                 dp[i][j] =[elem_1, elem_2, elem_3]
 
