c6c37c15eba2e4ce88b709608888f934577346fe
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-07-07 11:26:05 -0400

    algo

--- Python/3593. Minimum Increments to Equalize Leaf Paths.py
+++ Python/3593. Minimum Increments to Equalize Leaf Paths.py
@@ -70,3 +70,25 @@ class Solution:
                     count += 1
         return count - 1
                 
+class Solution:
+    def minIncrease(self, n: int, edges: List[List[int]], cost: List[int]) -> int:
+        G = [[] for i in range(n)]
+        for u, v in edges:
+            G[u].append(v)
+            G[v].append(u)
+        res = [0]
+
+        def dfs(i, f = -1):
+            score = []
+            for j in G[i]:
+                if j == f: continue
+                score.append(dfs(j, i))
+            if not score:
+                return cost[i]
+            ma = max(score)
+            res[0] += sum(ma - v > 0 for v in score)
+            return ma + cost[i]
+
+        dfs(0)
+        print(res)
+        return res[0]
