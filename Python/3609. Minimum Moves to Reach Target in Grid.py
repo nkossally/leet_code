cd37ed50845862d5a3fd5c75582f98401928c09d
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-07-11 20:19:55 -0400

    changes

--- Python/3609. Minimum Moves to Reach Target in Grid.py
+++ Python/3609. Minimum Moves to Reach Target in Grid.py
@@ -1,6 +1,7 @@
 import time
 
 class Solution:
+
     def minMoves(self, sx: int, sy: int, tx: int, ty: int) -> int:
         def helper(x, y, prev_x, prev_y, count):
             if x == tx and y == ty:
@@ -57,19 +58,59 @@ class Solution:
             count += 1
         
         return count 
-result =  Solution.minMovesFast(5, 1, 350824, 287008)
-    
-time_1 = time.time()
-for i in range(1000):
-    Solution.minMoves(5, 1, 350824, 287008)
-time_2 = time.time()
+    def minMovesFast_2(sx: int, sy: int, tx: int, ty: int) -> int:
+        count = 0
+        x = tx
+        y = ty
+        while x != sx or y != sy:
+            if x < sx or y < sy:
+                return -1
+            
+            if x > y:
+                if .5 * x > y:
+                    if x % 2 == 1:
+                        return -1
+                    x = .5 * x
+                else:
+                    x -= y
+            elif y > x:
+                if .5 * y > x:
+                    if y % 2 == 1:
+                        return -1
+                    y = .5 * y
+                else:
+                    y -= x
+            else:
+                if sx == 0:
+                    x = 0
+                elif sy == 0:
+                    y = 0
+                else:
+                    return -1
+            count += 1
+        
+        return count 
+instance = Solution()
+
+result =  instance.minMovesFast(5, 1, 350824, 287008)
+
+# time_1 = time.time()
+# for i in range(1):
+#     instance.minMoves(5, 1, 350824, 287008)
+# time_2 = time.time()
 
 time_3 = time.time()
 for i in range(1000):
-    Solution.minMovesFast(5, 1, 350824, 287008)
+    instance.minMovesFast(5, 1, 350824, 287008)
 time_4 = time.time()
+
+time_5 = time.time()
+for i in range(1000):
+    Solution.minMovesFast_2(5, 1, 350824, 287008)
+time_6 = time.time()
 
 
 print(f"result {result}")
-print(f"Slow wxecution time: {time_2 - time_1} seconds")
-print(f"Fast wxecution time: {time_4 - time_3} seconds")
+# print(f"Slow wxecution time: {time_2 - time_1} seconds")
+print(f"Instance Method wxecution time: {time_4 - time_3} seconds")
+print(f"Class wxecution time: {time_6 - time_5} seconds")
