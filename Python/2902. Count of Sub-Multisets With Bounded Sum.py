ddc93c7db14ad6eb34c12b0c368d5812e1075b6e
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-08-08 12:08:55 -0400

    algo

--- Python/2902. Count of Sub-Multisets With Bounded Sum.py
+++ Python/2902. Count of Sub-Multisets With Bounded Sum.py
@@ -17,3 +17,16 @@ class Solution:
                     dp[i] = get_mod(dp[i])
 
         return get_mod(sum(dp[l : r + 1]))
+
+    def countSubMultisetsFast(self, nums: List[int], l: int, r: int) -> int:
+        dp = [1] + [0] * r
+        counts = collections.Counter(nums)
+        zeros = counts.pop(0, 0) + 1
+        for a, c in counts.items():
+            stride_sums = dp[:]
+            for i in range(a, r + 1):
+                stride_sums[i] += stride_sums[i - a]
+            for k in range(r, 0, -1):
+                dp[k] = stride_sums[k] - stride_sums[k - a * c - a] if k >= a * c + a else stride_sums[k]
+
+        return zeros * sum(dp[l:r+1]) % (10 ** 9 + 7)
