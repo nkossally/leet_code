46d8859e066ec497bbeef55fa93fdf68ab2bf1c1
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-08-09 17:27:19 -0400

    algo

---
+++ Python/2896. Apply Operations to Make Two Strings Equal.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minOperations(self, s1: str, s2: str, x: int) -> int:
+        diffs = []
+        dp = {}
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                diffs.append(i)
+        if len(diffs) % 2 == 1:
+            return -1
+
+        def recur(diffs):
+            if len(diffs) == 0:
+                return 0
+
+            min_count = float("inf")
+
+            for i in range(len(diffs) - 1):
+                for j in range(i + 1, len(diffs)):
+                    diffs_cpy = diffs[:]
+                    cost = min(diffs[j] - diffs[i], x)
+                    diffs_cpy.pop(j)
+                    diffs_cpy.pop(i)
+                    min_count = min(min_count, cost + recur(diffs_cpy))
+            return min_count
+        res = recur(diffs)
+        return res 
