8753a6fe2979bee2f75f528b31ec97187e059180
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-08-18 12:23:51 -0400

    algo

--- Python/2845. Count of Interesting Subarrays.py
+++ Python/2845. Count of Interesting Subarrays.py
@@ -1,4 +1,24 @@
+from collections import defaultdict
+from typing import List
+
 class Solution:
+    def countInterestingSubarraysSlow(self, nums: List[int], modulo: int, k: int) -> int:
+        counts = [0]
+        count = 0
+        for num in nums:
+            if num % modulo == k:
+                count += 1
+            counts.append(count)
+
+        total = 0
+
+        for i in range(len(counts)):
+            for j in range(i + 1, len(counts)):
+                if (counts[j] - counts[i]) % modulo == k:
+                    total += 1
+
+        return total
+
     def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
         counts = defaultdict(int)
         # empty array increments counts[0]
