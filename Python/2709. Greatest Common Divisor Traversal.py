3f377373f64c66bc2acde629b243ed0983ce6836
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-09-20 20:48:09 -0400

    algo

--- Python/2709. Greatest Common Divisor Traversal.py
+++ Python/2709. Greatest Common Divisor Traversal.py
@@ -1,4 +1,42 @@
 class Solution:
+    def canTraverseAllPairsSlow(self, nums: List[int]) -> bool:
+        nums = list(set(nums))
+        def has_common_factor(a, b):
+            min_num = min(a, b)
+            max_num = max(a, b)
+            if max_num % min_num == 0:
+                return True
+            i = 2
+            while i <= min_num/2:
+                if min_num % i == 0 and max_num % i == 0:
+                    return True
+                i += 1
+            return False
+        common_divisors = defaultdict(set)
+        for i in range(len(nums)):
+            num_1 = nums[i]
+            common_divisors[num_1].add(num_1)
+            for j in range(i + 1, len(nums)):
+                num_2 = nums[j]
+                if has_common_factor(num_1, num_2):
+                    common_divisors[num_1].add(num_2)
+                    common_divisors[num_2].add(num_1)
+        stack = nums[:]
+        while stack:
+            new_stack = []
+            while stack:
+                num = stack.pop()
+                connections = common_divisors[num]
+                connections_cpy = list(connections)
+                for other_num in connections_cpy:
+                    other_num_connections = common_divisors[other_num]
+                    for second_connection in other_num_connections:
+                        if second_connection not in connections:
+                            connections.add(second_connection)
+                            new_stack.append(second_connection)
+            stack = new_stack
+        return len(common_divisors[nums[0]]) == len(nums)
+        
     def canTraverseAllPairs(self, nums: List[int]) -> bool:
         def dfs(index, visitedIndex, visitedPrime):
             if visitedIndex[index]:
