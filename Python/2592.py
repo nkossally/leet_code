f1d95d93e3988f3b4b753cfeae066a38a44bb7d3
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-10-25 17:07:19 -0400

    algo

--- Python/2592.py
+++ Python/2592.py
@@ -1,3 +1,24 @@
+
+class Solution:
+    def maximizeGreatness(self, nums: List[int]) -> int:
+        cpy = nums[:]
+        cpy.sort()
+        nums.sort()
+        self.idx = 0
+        def get_idx(val):
+            while self.idx < len(cpy):
+                if cpy[self.idx] > val:
+                    return 
+                self.idx += 1
+        ans = 0
+        for num in nums:
+            get_idx(num)
+            if self.idx != len(cpy):
+                ans += 1
+                cpy.pop(self.idx)
+        return ans
+
+        
 class Solution:
     def maximizeGreatnessFast(self, nums: List[int]) -> int:
         nums.sort()
