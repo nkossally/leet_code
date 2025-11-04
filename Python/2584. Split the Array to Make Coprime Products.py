c16578d08cbac96138265a9455f8b67d34333bb7
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-11-04 06:05:38 -0500

    algo

--- Python/2584. Split the Array to Make Coprime Products.py
+++ Python/2584. Split the Array to Make Coprime Products.py
@@ -24,4 +24,35 @@ class Solution:
             if gcd == 1:
                 return i
         return -1
+    
+    def findValidSplit_2(self, nums: List[int]) -> int:
+        right_indices = {}
+        factors_at_idx = [ [] for i in range(len(nums)) ]
+
+        def record_prime_idx(num, idx):
+            max_factor = floor(pow(num, .5))
+            factor = 2
+            while num > 1 and factor <= num:
+                if num % factor == 0:
+                    factors_at_idx[idx].append(factor)
+                    if factor not in right_indices:
+                        right_indices[factor] = idx
+                    else:
+                        right_indices[factor]= max(right_indices[factor], idx)
+                    while num % factor == 0:
+                        num /= factor
+                factor += 1
+        right_bound = -1
+        for i, num in enumerate(nums):
+            record_prime_idx(num, i)
+            
+        for i in range(len(nums) - 1):        
+            factor_bound = -1
+            for factor in factors_at_idx[i]:
+                factor_bound = max(factor_bound, right_indices[factor])
+            right_bound = max(right_bound, factor_bound)
+            if right_bound == i:
+                return i
+        return - 1
+            
         
