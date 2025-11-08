5d45376c524147b155beedb61f630fd24f0dd83f
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-11-07 19:06:10 -0500

    algo

--- Python/2585. Number of Ways to Earn Points.py
+++ Python/2585. Number of Ways to Earn Points.py
@@ -19,3 +19,23 @@ class Solution:
         get_to_target(0, 0)
 
         return self.res
+
+    def waysToReachTargetFast(self, target: int, types: List[List[int]]) -> int:
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
+        def get_mod(num):
+            return num % (10 **9 + 7)
+
+        for count, num in types:
+            cpy = dp[:]
+            for prev in range(0, target):
+                if cpy[prev] == 0:
+                    continue
+                for factor in range(1, count + 1):
+                    total = prev + factor * num
+                    if total <= target:
+                        dp[total] += cpy[prev]
+                        dp[total] = get_mod(dp[total])
+
+        return dp[target]
