57457730f501c59b2585ffce179f7ca9ce3b8153
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-11-12 03:45:07 -0500

    algo

--- Python/2577. Minimum Time to Visit a Cell In a Grid.py
+++ Python/2577. Minimum Time to Visit a Cell In a Grid.py
@@ -51,4 +51,36 @@ class Solution:
                         heapq.heappush(new_queue, [grid[x_2][y_2], x_2, y_2])
             time += 1
             queue = new_queue
-                
+
+
+    def minimumTimeBetter(self, grid: List[List[int]]) -> int:
+        if grid[0][1] > 1 and grid[1][0] > 1:
+            return -1
+        
+        queue = [[0, 0, 0]]
+        dirs = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+        visited = set()
+        
+        def is_on_board(x, y):
+            return x >= 0 and x < len(grid) and y >= 0 and y < len(grid[0])
+        
+        while queue:
+            time, x, y = heapq.heappop(queue)
+            if (x, y) in visited:
+                continue
+            visited.add((x, y))
+           
+            if x == len(grid) - 1 and y == len(grid[0]) -1:
+                return time
+            
+            for dir in dirs:
+                x_2 = x + dir[0]
+                y_2 = y + dir[1]
+                if is_on_board(x_2, y_2) and (x_2, y_2) not in visited:
+                    new_time = time + 1
+                    if new_time < grid[x_2][y_2]:
+                        diff = grid[x_2][y_2] - new_time
+                        if diff % 2 == 1:
+                            new_time =  grid[x_2][y_2] + 1
+                        else:
+                            new_time = grid[x_2][y_2]
