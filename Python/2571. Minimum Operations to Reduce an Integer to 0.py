b2d3a5a0269d49777f7067481a247fead08e3099
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-11-14 01:42:53 -0500

    algo

--- Python/2571. Minimum Operations to Reduce an Integer to 0.py
+++ Python/2571. Minimum Operations to Reduce an Integer to 0.py
@@ -36,4 +36,17 @@ class Solution:
                 res += 1
                 n >>= 2
         return res
-            
+    
+    def minOperationsFast_2( self, n: int) -> int:
+        res = 0
+        while n > 0:
+            if n % 2 == 0:
+                n >>= 1
+            elif (n & 2) > 0:
+                n += 1
+                res += 1
+            else:
+                res += 1
+                n -= 1
+                # n >>= 2
+        return res
