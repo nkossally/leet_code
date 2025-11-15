0a5698691ed2d10cc2fd43da4add6eb041358bad
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-11-15 06:44:03 -0500

    algo

--- Python/2572. Count the Number of Square-Free Subsets.py
+++ Python/2572. Count the Number of Square-Free Subsets.py
@@ -1,3 +1,6 @@
+from typing import List
+from math import floor, gcd
+from collections import defaultdict
 class Solution:
     def squareFreeSubsets(self, nums: List[int]) -> int:
         def divisible_by_square(num):
@@ -19,7 +22,42 @@ class Solution:
                 get_subset(new_prod, idx + 1)
             get_subset(product, idx + 1)
         get_subset(1, 0)
+        print(self.res)
         return self.res
- 
+    
+    def squareFreeSubsetsFast(self, nums: List[int]) -> int:
+        def get_mod(num):
+            return num % (10**9 + 7)
 
+        candidates = set([1, 2, 3, 5, 6, 7, 10, 11, 13, 14, 15, 17, 19, 21, 22, 23, 26, 29, 30])
+        cnt = defaultdict(int)
+        for num in nums:
+            if num in candidates:
+                cnt[num] += 1
         
+        self.res = -1
+
+        unique_nums = list(cnt.keys())
+
+        def get_subset(prod, factor, idx):
+            # print(prod, factor, idx)
+            if idx == len(unique_nums):
+                self.res += factor
+                self.res = get_mod(self.res)
+                return
+            
+            num = unique_nums[idx]
+            count = cnt[num]
+
+            if  gcd(num, prod) == 1:
+                mult = count
+                if num == 1:
+                    mult = 2**count - 1
+                get_subset(prod * num, factor * mult, idx + 1)
+            get_subset(prod, factor, idx + 1)
+        get_subset(1, 1, 0)
+        print(self.res)
+        return self.res
+
+Solution().squareFreeSubsetsFast([1, 1, 3, 3])
+Solution().squareFreeSubsets([1, 1, 3, 3])
