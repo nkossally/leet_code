c61c0acc33cf4a3e773ba658c8f1a15622ac9c4f
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-12-16 17:15:09 -0500

    algo

--- Python/2538. Difference Between Maximum and Minimum Price Sum.py
+++ Python/2538. Difference Between Maximum and Minimum Price Sum.py
@@ -21,7 +21,7 @@ class Solution:
                 path_sum = max(res, get_max_path(num, set([num])) )
                 res = max(res, path_sum - price[num])
         return res
-    
+
     def maxOutputMem(self, n: int, edges: List[List[int]], price: List[int]) -> int:
         node_to_edge = defaultdict(set)
         mem = [{} for _ in range(n)]
@@ -30,17 +30,15 @@ class Solution:
             node_to_edge[a].add(b)
             node_to_edge[b].add(a)
         
-        def get_max_path(node, seen):
+        def get_max_path(node, prev):
 
             max_tail = 0
             for adj in node_to_edge[node]:
-                if adj not in seen:
+                if adj != prev:
                     if adj in mem[node]:
                         max_tail = max(max_tail, mem[node][adj])
                     else:
-                        seen_copy = seen.copy()
-                        seen_copy.add(adj)
-                        tail = get_max_path(adj, seen_copy)
+                        tail = get_max_path(adj, node)
                         mem[node][adj] = tail
                         max_tail = max(max_tail, tail)
             return max_tail + price[node]
@@ -48,6 +46,6 @@ class Solution:
         res = 0
         for num in range(n):
             if len(node_to_edge[num]) == 1:
-                path_sum = max(res, get_max_path(num, set([num])) )
+                path_sum = max(res, get_max_path(num, None) )
                 res = max(res, path_sum - price[num])
         return res
