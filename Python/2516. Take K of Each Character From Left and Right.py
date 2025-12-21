648134e53d444a602ec35b8c203fbc65da339daf
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-12-21 16:15:23 -0500

    algo

---
+++ Python/2516. Take K of Each Character From Left and Right.py
@@ -0,0 +1,60 @@
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        if k == 0:
+            return 0
+        a_left = []
+        a_right = []
+        b_left = []
+        b_right = []
+        c_left = []
+        c_right = []
+        a_left_count = 0
+        a_right_count = 0
+        b_left_count = 0
+        b_right_count = 0
+        c_left_count = 0
+        c_right_count = 0
+
+
+        for i in range(len(s)):
+            j = len(s) - 1 - i
+            if s[i] == "a":
+                a_left_count += 1 
+            elif s[i] == "b":
+                b_left_count += 1
+            else:
+                c_left_count += 1
+
+            if s[j] == "a":
+                a_right_count += 1 
+            elif s[j] == "b":
+                b_right_count += 1
+            else:
+                c_right_count += 1
+            
+            a_left.append(a_left_count)
+            a_right.insert(0, a_right_count)
+            b_left.append(b_left_count)
+            b_right.insert(0, b_right_count)
+            c_left.append(c_left_count)
+            c_right.insert(0, c_right_count)
+
+        
+        if a_left[-1] < k or b_left[-1] < k or c_left[-1] < k:
+            return -1
+        min_count = float("inf")
+        for i in range(len(s)):
+            if a_left[i] >= k and b_left[i] >= k and c_left[i] >= k:
+                min_count = min(min_count, i + 1)
+            if a_right[i] >= k and b_right[i] >= k and c_right[i] >= k:
+                min_count = min(min_count, len(s) - i)
+            for j in range(i + 1, len(s)):
+                a_total = a_left[i] + a_right[j]
+                b_total = b_left[i] + b_right[j]
+                c_total = c_left[i] + c_right[j]
+                if a_total >=k and b_total >= k and c_total >=k:
+                    # print("i", i, "j", j, "a_total", a_total, a_total, a_total)
+                    count = i + 1 + len(s) - j
+                    min_count = min(min_count, count)
+
+        return -1 if min_count == float("inf") else min_count
--- _general_algo_references/sieve-primes.py
+++ _general_algo_references/sieve-primes.py
@@ -13,36 +13,3 @@ def get_primes(self, left: int, right: int) -> List[int]:
     
     primes = [i for i in range(left, right + 1) if sieve[i]]
     return primes
-
-def get_primes_slow(self, left: int, right: int) -> List[int]:
-    primes = []
-
-    def is_prime(num):
-        if num == 1:
-            return False
-        curr = floor(num ** .5)
-        while curr > 1:
-            if num % curr == 0:
-                return False
-            curr -= 1
-        return True
-    for num in range(left, right + 1):
-        if is_prime(num):
-            primes.append(num)
-    return primes
-    
-for _ in range(100):
-    start = perf_counter()
-    get_primes(None, 10, 2000)
-    end = perf_counter()
-    print(end - start)
-
-for _ in range(100):
-    start = perf_counter()
-    get_primes(None, 10, 2000)
-    end = perf_counter()
-    print(end - start)
-
-
-print(get_primes(None, 10, 2000)) 
-print(get_primes_slow(None, 10, 2000))  # Example usage # Example usage
