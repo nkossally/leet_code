8bbc18e6141bd96f99fc6f02e247b26fc11a5feb
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-12-21 01:07:46 -0500

    algo

---
+++ Python/2522. Partition String Into Substrings With Values at Most K.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumPartition(self, s: str, k: int) -> int:
+        mem = [{} for _ in range(len(s))]
+
+        def recur(idx, num_str):
+            if len(num_str) and int(num_str) > k:
+                return float("inf")
+            if idx == len(s):
+                increment = 1 if len(num_str) else 0
+                return increment
+            if num_str in mem[idx]:
+                return mem[idx][num_str]
+            way_1 = float("inf")
+
+            if len(num_str):
+                way_1 = 1 + recur( idx +1, s[idx])
+            way_2 = recur(idx +1, num_str[:] + s[idx])
+            min_way = min(way_1, way_2)
+            mem[idx][num_str] = min_way
+            return min_way
+            
+        res = recur(0, "")
+        return res if res != float("inf") else -1
