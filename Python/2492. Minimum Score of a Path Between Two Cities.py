344f34811bfad8c3afa6bf0639fbac7d8d23a7c8
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2026-02-14 16:33:39 -0500

    algo

--- Python/2492. Minimum Score of a Path Between Two Cities.py
+++ Python/2492. Minimum Score of a Path Between Two Cities.py
@@ -21,3 +21,29 @@ class Solution:
                     
         return roads_dict[1][n]
         
+    def minScore2(self, n: int, roads: List[List[int]]) -> int:
+        roads_dict = defaultdict(lambda: defaultdict(lambda: float("inf")))
+        max_cost = 0
+        min_1_cost = float("inf")
+        for road_1, road_2, dist in roads:
+            roads_dict[road_1][road_2] = dist
+            roads_dict[road_2][road_1] = dist
+            max_cost = max(max_cost, dist)
+            if road_1 == 1 or road_2 == 1:
+                min_1_cost = min(min_1_cost, dist)
+
+        res = float("inf")
+        stack = [ [n, max_cost, set([n])]]
+        while stack:
+            node, cost, seen = stack.pop()
+
+            if node == 1:
+                res = min(res, cost)
+                continue
+            for other_node, other_dist  in roads_dict[node].items():
+                if other_node not in seen:
+                    seen_cpy = seen.copy()
+                    seen_cpy.add(other_node)
+                    resolved_dist = other_dist if other_node != 1 else min_1_cost
+                    stack.append([other_node, min(cost, resolved_dist), seen_cpy])
+        return res
