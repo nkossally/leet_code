4787436965f37aaec8f55c202f37cbf2773ce2d8
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2026-02-23 09:17:20 -0500

    algo

--- Python/2493. Divide Nodes Into the Maximum Number of Groups.py
+++ Python/2493. Divide Nodes Into the Maximum Number of Groups.py
@@ -1,4 +1,6 @@
 import copy
+from collections import defaultdict, deque
+from typing import List
 
 class Solution:
     def magnificentSets(self, n: int, edges: List[List[int]]) -> int:
@@ -7,12 +9,17 @@ class Solution:
             connections[a].append(b)
             connections[b].append(a)
         
-        def recur(node, groups):
-            if node == n + 1:
+        nodes = list(range(1, n + 1))
+        nodes.sort(key = lambda node: -len(connections[node]))
+        
+        def recur(node_idx, groups):
+            print(f"node_idx: {node_idx}, groups: {groups}")
+            
+            if node_idx == n:
                 return len(groups)
+            node = nodes[node_idx]
             
             positions = []
-            bad_positions = set()
 
             for other in connections[node]:
                 for idx, group in enumerate(groups):
@@ -40,10 +47,41 @@ class Solution:
                     cpy.append([node])
                 else:
                     cpy[pos].append(node)
-                res = max(res, recur(node + 1, cpy))
+                res = max(res, recur(node_idx + 1, cpy))
 
             return res
-        res = recur(1, [])
+        res = recur(0, [])
         if res == float("-inf"):
             return -1
         return res
+
+
+    def magnificentSetsFast(self, n: int, edges: List[List[int]]) -> int:
+        g = [[] for _ in range(n)]
+        for a, b in edges:
+            g[a - 1].append(b - 1)
+            g[b - 1].append(a - 1)
+        d = defaultdict(int)
+        for i in range(n):
+            q = deque([i])
+            dist = [0] * n
+            dist[i] = mx = 1
+            root = i
+            while q:
+                a = q.popleft()
+                root = min(root, a)
+                for b in g[a]:
+                    if dist[b] == 0:
+                        dist[b] = dist[a] + 1
+                        mx = max(mx, dist[b])
+                        q.append(b)
+                    elif abs(dist[b] - dist[a]) != 1:
+                        return -1
+            d[root] = max(d[root], mx)
+        return sum(d.values())
+
+n = 92
+
+edges = [[67,29],[13,29],[77,29],[36,29],[82,29],[54,29],[57,29],[53,29],[68,29],[26,29],[21,29],[46,29],[41,29],[45,29],[56,29],[88,29],[2,29],[7,29],[5,29],[16,29],[37,29],[50,29],[79,29],[91,29],[48,29],[87,29],[25,29],[80,29],[71,29],[9,29],[78,29],[33,29],[4,29],[44,29],[72,29],[65,29],[61,29]]
+
+print(Solution().magnificentSetsFast(92, edges))
