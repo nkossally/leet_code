9995d37e00c08f172b19ac0452cb09e6071c1224
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2026-04-11 11:40:01 -0400

    algo

--- Python/2472. Maximum Number of Non-overlapping Palindrome Substrings.py
+++ Python/2472. Maximum Number of Non-overlapping Palindrome Substrings.py
@@ -39,3 +39,34 @@ class Solution(object):
             groups = groups + new_groups
 
         return ans
+    
+
+    def maxPalindromesFast(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+    
+        n = len(s)
+        # function to check whether substring is a palindrome
+        def valid(i, j):
+            # if end index is greater then length of string
+            if j > len(s):
+                return False
+            if s[i : j] == s[i : j][::-1]:
+                return True
+            return False
+        maxSubstrings = 0
+        start = 0
+        while start < n:
+            if valid(start, start + k):
+                maxSubstrings += 1
+                start += k
+            elif valid(start, start + k + 1):
+                maxSubstrings += 1
+                start += k + 1
+            else:
+                # when there is no palindrome starting at that particular index   
+                start += 1
+        return maxSubstrings
