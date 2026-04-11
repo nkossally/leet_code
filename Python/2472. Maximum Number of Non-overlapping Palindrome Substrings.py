415d82a06c8610cb9aa9751aeb52d3e8b3ea9e1f
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2026-04-10 21:18:33 -0400

    algo

--- Python/2472. Maximum Number of Non-overlapping Palindrome Substrings.py
+++ Python/2472. Maximum Number of Non-overlapping Palindrome Substrings.py
@@ -1,3 +1,5 @@
+import copy
+
 class Solution(object):
     def maxPalindromes(self, s, k):
         """
@@ -12,6 +14,28 @@ class Solution(object):
             r = len(string) - 1
             while l < r and string[l] == string[r]:
                 l += 1
-                r += 1
+                r -= 1
             return r <= l
-        
+        def can_add_interval(group, interval):
+            for other in group:
+                if do_intervals_overlap(other, interval):
+                    return False
+            return True
+        intervals = []
+        for i in range(len(s)):
+            for j in range(i + k - 1, len(s)):
+                if get_is_palindrome(s[i : j + 1]):
+                    intervals.append([i, j])
+        ans = 0
+        groups = [[]]
+        for interval in intervals:
+            new_groups = []
+            for group in groups:
+                if can_add_interval(group, interval):
+                    cpy = copy.deepcopy(group)
+                    cpy.append(interval)
+                    new_groups.append(cpy)
+                    ans = max(ans, len(cpy))
+            groups = groups + new_groups
+
+        return ans
